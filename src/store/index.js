82af5123927fbe4286a7a8e199142418a4905b1b
love1234 <345841634@qq.com>
2019-05-20 17:13:11 +0800

    xx

--- src/store/index.js
+++ src/store/index.js
@@ -1,32 +1,32 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import {getAdminInfo} from '@/api/getData'
+import { getAdminInfo } from '@/api/getData'
 
 Vue.use(Vuex)
 
 const state = {
 	adminInfo: {
 		avatar: 'http://static.kuaimayoupin.com/16a17111a44.png',
-		id:null
+		id: null
 	},
 }
 
 const mutations = {
-	saveAdminInfo(state, adminInfo){
+	saveAdminInfo(state, adminInfo) {
 		state.adminInfo = adminInfo;
 	}
 }
 
 const actions = {
-	async getAdminData({commit}){
-		try{
+	async getAdminData({ commit }) {
+		try {
 			const res = await getAdminInfo()
 			if (res.status == 1) {
 				commit('saveAdminInfo', res.data);
-			}else{
+			} else {
 				throw new Error(res.type)
 			}
-		}catch(err){
+		} catch (err) {
 			// console.log(err.message)
 		}
 	}
